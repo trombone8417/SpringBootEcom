d15f44142a51e7c17f6ce09336f2cc653031ee3e
trombone8417 <trombone8417@yahoo.com>
2021-05-10 15:57:30 +0800

    dao & entity

---
+++ backend/src/main/java/com/kuei/ecom/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kuei.ecom.dao;
+
+import com.kuei.ecom.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
