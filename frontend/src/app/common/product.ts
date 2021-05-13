2e2ad4c1e8727c9d231aeef0166e2f2c4517db2f
trombone8417 <trombone8417@yahoo.com>
2021-05-14 07:40:17 +0800

    product-list component

---
+++ frontend/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInSotck: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
