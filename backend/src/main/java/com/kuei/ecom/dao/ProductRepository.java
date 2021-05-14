d8bd38562c002ee1d60795cabd109b52b5a316b0
trombone8417 <trombone8417@yahoo.com>
2021-05-15 05:55:44 +0800

    product service

--- backend/src/main/java/com/kuei/ecom/dao/ProductRepository.java
+++ backend/src/main/java/com/kuei/ecom/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.kuei.ecom.dao;
 
 import com.kuei.ecom.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
