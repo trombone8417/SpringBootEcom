6ba45318b68c12dc43e7717b088c072fddce858d
kuei <trombone8417@yahoo.com>
2021-05-25 07:39:56 +0800

    app-search

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -11,6 +11,7 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
@@ -22,6 +23,22 @@ export class ProductListComponent implements OnInit {
     });
   }
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+
+  }
+  handleSearchProducts(){
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    this.productService.searchProducts(theKeyword).subscribe((data) => {
+      this.products = data;
+    });
+  }
+  handleListProducts(){
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -11,16 +11,26 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
 
+
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient:HttpClient) { }
 
-  getProductList(theCategoryId:number): Observable<Product[]>{
-    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
-    )
+    );
+  }
+
+  getProductList(theCategoryId:number): Observable<Product[]>{
+    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+    return this.getProducts(searchUrl)
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+    return this.getProducts(searchUrl)
   }
 
   getProductCategories():Observable<ProductCategory[]> {
