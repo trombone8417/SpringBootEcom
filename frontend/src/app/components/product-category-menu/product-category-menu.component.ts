65d89a2bc95befd55e26a3681eb690aa14e7f15a
kuei <trombone8417@yahoo.com>
2021-05-24 08:21:26 +0800

    app-product-category-menu

--- frontend/src/app/components/product-category-menu/product-category-menu.component.ts
+++ frontend/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-category-menu',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCategoryMenuComponent implements OnInit {
 
-  constructor() { }
+  productCategories: ProductCategory[];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProductCategories();
+
+  }
+  listProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      data=>{
+        console.log('Product Categories='+JSON.stringify(data));
+        this.productCategories = data;
+      }
+    )
   }
 
 }
