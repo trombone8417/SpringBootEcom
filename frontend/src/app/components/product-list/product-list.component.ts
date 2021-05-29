23cd5977d386fd0a1d52694778e387ed0d477f83
kuei <trombone8417@yahoo.com>
2021-05-30 06:00:57 +0800

    CartService

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -100,4 +100,8 @@ export class ProductListComponent implements OnInit {
     this.thePageNumber=1;
     this.listProducts();
   }
+
+  addToCart(theProduct: Product){
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
 }
---
+++ frontend/src/app/services/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() {}
+
+  addToCart(theCartItem: CartItem) {
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+      alreadyExistsInCart = existingCartItem != undefined;
+    }
+    if (alreadyExistsInCart) {
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(theCartItem);
+    }
+  }
+}
