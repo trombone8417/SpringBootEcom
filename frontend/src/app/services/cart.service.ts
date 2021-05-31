3c9edb12cbe528a0bf2b2d8db479c6bf7e93657f
kuei <trombone8417@yahoo.com>
2021-05-31 13:04:00 +0800

    CartDetails

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { CartItem } from '../common/cart-item';
   providedIn: 'root',
 })
 export class CartService {
+
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
@@ -55,5 +56,22 @@ export class CartService {
       )},totalQuantity: ${totalQuantityValue}`
     );
     console.log('----');
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity===0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id ===theCartItem.id);
+
+    if (itemIndex>-1) {
+      this.cartItems.splice(itemIndex,1);
+      this.computeCartTotals();
+    }
   }
 }
