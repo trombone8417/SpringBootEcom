7fa557b8cf12891c299e41dea7f17af68a213010
kuei <trombone8417@yahoo.com>
2021-06-10 07:46:44 +0800

    members-page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,17 +17,20 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
-import {OKTA_CONFIG,OktaAuthModule,OktaCallbackComponent} from '@okta/okta-angular';
+import {OKTA_CONFIG,OktaAuthModule,OktaCallbackComponent, OktaAuthGuard} from '@okta/okta-angular';
 import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = Object.assign({
-  onAuthRequired:(injector)=>{
+  onAuthRequired:(oktaAuth,injector)=>{
     const router = injector.get(Router);
     router.navigate(['/login']);
   }
 },myAppConfig.oidc);
 
 const routes:Routes = [
+
+  {path:'members',component: MembersPageComponent, canActivate:[OktaAuthGuard]},
   {path:'login/callback',component: OktaCallbackComponent},
   {path:'login',component: LoginComponent},
   {path:'checkout',component: CheckoutComponent},
@@ -52,7 +55,8 @@ const routes:Routes = [
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
