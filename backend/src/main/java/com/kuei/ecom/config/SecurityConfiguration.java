6f7a5681a500b1126947ce1d2e5e368a9199af93
kuei <trombone8417@yahoo.com>
2021-06-12 08:52:59 +0800

    SecurityConfiguration

--- backend/src/main/java/com/kuei/ecom/config/SecurityConfiguration.java
+++ backend/src/main/java/com/kuei/ecom/config/SecurityConfiguration.java
@@ -1,2 +1,22 @@
-package com.kuei.ecom.config;public class SecurityConfiguration {
+package com.kuei.ecom.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        http.cors();
+        Okta.configureResourceServer401ResponseBody(http);
+    }
 }
